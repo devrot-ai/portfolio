32bdd944c4c16daec50734d5623a3f89d91e87d6
kanishk <kanishkthakur12006@gmail.com>
2026-07-07 02:06:09 +0530

    feat: wire resume button to download PDF

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -68,7 +68,7 @@ const SocialIcons = () => {
           </a>
         </span>
       </div>
-      <a className="resume-button" href="#">
+      <a className="resume-button" href="/Kanishk_Chhoker_Resume.pdf" download="Kanishk_Chhoker_Resume.pdf">
         <HoverLinks text="RESUME" />
         <span>
           <TbNotes />
